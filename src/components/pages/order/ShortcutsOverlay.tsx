c3b35b9f5ed244b4adde1cd49efea02e46e1dcdb
duftor <duftor@outlook.fr>
2025-09-17 16:54:49 +0200

    feat: Create ShortcutsOVerlay layout

---
+++ src/components/pages/order/ShortcutsOverlay.tsx
@@ -0,0 +1,59 @@
+import { theme } from "@/theme/theme"
+import styled from "styled-components"
+
+export function ShortcutsOverlay() {
+	return (
+		<ShortcutsOverlayStyled>
+			<div className="shortcuts-wrapper">
+				<div className="bold">💡 Pour aller plus vite :</div>
+				<div>⌘ + i : Toggle "mode" admin</div>
+				<div>⌘ + j : Toggle "panel" admin</div>
+				<button className="button">Ne plus afficher</button>
+			</div>
+		</ShortcutsOverlayStyled>
+	)
+}
+
+const ShortcutsOverlayStyled = styled.div`
+	background-color: ${theme.colors.dark};
+	border-radius: ${theme.borderRadius.round};
+
+	position: absolute;
+	top: 0;
+	left: 0;
+	margin: 20px;
+
+	.shortcuts-wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: ${theme.spacing.sm};
+		padding: 20px;
+
+		color: ${theme.colors.white};
+		font-size: ${theme.fonts.size.SM};
+
+		.bold {
+			font-weight: ${theme.fonts.weights.bold};
+			font-size: ${theme.fonts.size.P0};
+		}
+
+		.button {
+			background-color: ${theme.colors.dark};
+			color: ${theme.colors.white};
+			border: 1px solid ${theme.colors.white};
+			border-radius: 25px;
+			padding: 13px 0px;
+			cursor: pointer;
+
+			&.button:hover {
+				color: ${theme.colors.primary};
+				border-color: ${theme.colors.primary};
+			}
+
+			&.button:active {
+				color: ${theme.colors.primary_darken};
+				border-color: ${theme.colors.primary_darken};
+			}
+		}
+	}
+`
