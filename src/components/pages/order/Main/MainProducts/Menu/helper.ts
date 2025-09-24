a302400abb21c3de24e95eed414b05060b386847
duftor <duftor@outlook.fr>
2025-09-24 22:43:25 +0200

    [PATCH] TSE-23 : ORDER > CARD > afficher les categories (part 2/2)

--- src/components/pages/order/Main/MainProducts/Menu/helper.ts
+++ src/components/pages/order/Main/MainProducts/Menu/helper.ts
@@ -1,6 +1,12 @@
-export const checkIfProductIsClicked = (
-  idProductInMenu: string,
-  idProductClickedOn: string
-): boolean => {
-  return idProductInMenu === idProductClickedOn
+export const checkIfProductIsClicked = (idProductInMenu: any, idProductClickedOn: any): any => {
+	return idProductInMenu === idProductClickedOn
+}
+
+export const getProductsToDisplay = (categoryAll: any, products: any, activeCategory: any) => {
+	const productsToDisplayed = categoryAll.isActive
+		? products
+		: products.filter(({ categories: categoriesFromMenu }) =>
+				categoriesFromMenu?.some((categoryFromMenu) => categoryFromMenu.label === activeCategory?.label)
+		  )
+	return productsToDisplayed
 }
