aa711953d94ffd74c5780e04783e25c343a9c6fa
duftor <duftor@outlook.fr>
2025-10-07 19:26:42 +0200

    feat(TSE-26): Create preview

--- src/components/pages/order/Main/MainProducts/Admin/AdminPanel/Form/CategoryPreview.tsx
+++ src/components/pages/order/Main/MainProducts/Admin/AdminPanel/Form/CategoryPreview.tsx
@@ -1,5 +1,16 @@
-type CategoryPreviewProps = {}
+import { Chip } from "@/components/reusable-ui/Chip"
+import { Preview } from "./Preview"
+import { Category } from "@/types/Category"
+import { theme } from "@/theme/theme"
 
-export const CategoryPreview = ({}: CategoryPreviewProps) => {
-	return <div>CategoryPreview</div>
+type CategoryPreviewProps = {
+	entity: Category
+}
+
+export const CategoryPreview = ({ entity }: CategoryPreviewProps) => {
+	return (
+		<Preview>
+			{entity.label ? <Chip {...entity} /> : <span style={{ color: theme.colors.greyBlue }}>Aucune preview</span>}
+		</Preview>
+	)
 }
--- src/components/pages/order/Main/MainProducts/Admin/AdminPanel/Form/ImagePreview.tsx
+++ src/components/pages/order/Main/MainProducts/Admin/AdminPanel/Form/ImagePreview.tsx
@@ -1,6 +1,7 @@
 import { fadeIn } from "@/theme/animations"
 import { theme } from "@/theme/theme"
 import styled from "styled-components"
+import { Preview } from "./Preview"
 
 type ImagePreviewProps = {
 	entity: {
@@ -12,18 +13,15 @@ type ImagePreviewProps = {
 export function ImagePreview({ entity }: ImagePreviewProps) {
 	const { imageSource, title } = entity
 	return (
-		<ImagePreviewStyled>
-			{imageSource ? <img src={imageSource} alt={title} /> : <div className="empty-image">Aucune Image</div>}
-		</ImagePreviewStyled>
+		<Preview withBorder={!imageSource}>
+			<ImagePreviewStyled>
+				{imageSource ? <img src={imageSource} alt={title} /> : <div className="empty-image">Aucune Image</div>}
+			</ImagePreviewStyled>
+		</Preview>
 	)
 }
 
 const ImagePreviewStyled = styled.div`
-	grid-area: 1 / 1 / 4 / 2;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-
 	img {
 		width: 100px;
 		height: 100px;
@@ -33,14 +31,6 @@ const ImagePreviewStyled = styled.div`
 	}
 
 	.empty-image {
-		height: 100%;
-		width: 100%;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		border: 1px solid ${theme.colors.greyLight};
-		line-height: 1.5;
 		color: ${theme.colors.greySemiDark};
-		border-radius: ${theme.borderRadius.round};
 	}
 `
